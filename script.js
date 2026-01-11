93b68e81af73fea1da6f2ea27d57e9dda2e32a81
Rishabh Maru <148260712+Rishabhmaru01@users.noreply.github.com>
2026-01-11 21:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+const input = document.getElementById("newTodoInput");
+const button = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+button.addEventListener("click", function () {
+  const todoText = input.value.trim(); // remove extra spaces
+
+  // prevent empty submission
+  if (todoText === "") {
+    return;
+  }
+
+  // create new list item
+  const li = document.createElement("li");
+  li.textContent = todoText;
+
+  // add li to ordered list
+  todoList.appendChild(li);
+
+  // clear input after adding todo
+  input.value = "";
+});
